8b333c1df6335e78c2b9074c84c077d3d0e9c42a
JBS <jebr@users.noreply.github.com>
2019-03-20 11:23:57 +0100

    Add: Enable RDP

--- SSP_v1.3.0.1.cmd
+++ SSP_v1.3.0.1.cmd
@@ -536,7 +536,9 @@ title RDP activeren
 echo.
 echo RDP wordt geactiveerd.
 
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f >nul
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v SecurityLayer /t REG_DWORD /d 0 /f >nul
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f >nul
 
 color 0A
 cls
